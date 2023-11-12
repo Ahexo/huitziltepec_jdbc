7e4d7a63a98bfaacfe942aa9a9354276605f0a93
Jorge Velasco García <ge0rg1vg@MBPJorge.local>
2023-11-11 22:11:12 -0600

    docs

--- SRC/Proyecto/src/main/java/com/excentos/zoohuitziltepec/Controller/ClienteControlador.java
+++ SRC/Proyecto/src/main/java/com/excentos/zoohuitziltepec/Controller/ClienteControlador.java
@@ -27,26 +27,46 @@ public class ClienteControlador {
    @Resource
    ClienteServicio clienteServ;
    
+   /**
+    * Obtiene todos los clientes registrados en la base de datos.
+    * @return Lista de clientes.
+    */
    @GetMapping(value = "/clienteList")
    public List<Cliente> getCliente(){
        return clienteServ.findAll();
    }
    
+   /**
+    * Inserta un nuevo cliente en la base de datos.
+    * @param cliente Cliente a insertar.
+    */
    @PostMapping(value = "/createCliente")
    public void createCliente(@RequestBody Cliente cliente){
        clienteServ.insertCliente(cliente);
    }
    
+    /**
+     * Emite una actualización de un cliente en la base de datos.
+     * @param cliente Cliente a actualizar.
+     */
    @PutMapping(value = "/updateCliente")
    public void updateCliente(@RequestBody Cliente cliente){
        clienteServ.updateCliente(cliente);
    }
    
+    /**
+     * Ejecuta una operación de actualización de un cliente en la base de datos.
+     * @param cliente Cliente a actualizar.
+     */
    @PutMapping(value = "/executeUpdateCliente")
    public void executeUpdateCliente(@RequestBody Cliente cliente){
        clienteServ.executeUpdateCliente(cliente);
    }
    
+    /**
+     * Borra un cliente de la base de datos.
+     * @param cliente Cliente a borrar.
+     */
    @DeleteMapping(value = "/deleteClienteById")
    public void borrarCliente(@RequestBody Cliente cliente){
        clienteServ.deleteCliente(cliente);
--- SRC/Proyecto/src/main/java/com/excentos/zoohuitziltepec/Service/VeterinarioServicio.java
+++ SRC/Proyecto/src/main/java/com/excentos/zoohuitziltepec/Service/VeterinarioServicio.java
@@ -12,9 +12,34 @@ import java.util.List;
  * @author ahexo
  */
 public interface VeterinarioServicio {
+
+    /**
+     * Obtiene todos los veterinarios registrados en la base de datos.
+     * @return Lista de veterinarios.
+     */
     List<Veterinario> findAll();
+
+    /**
+     * Inserta un nuevo veterinario en la base de datos.
+     * @param veterinario Veterinario a insertar.
+     */
     void insertVeterinario(Veterinario veterinario);
+
+    /**
+     * Emite una actualización de un veterinario en la base de datos.
+     * @param veterinario Veterinario a actualizar.
+     */
     void updateVeterinario(Veterinario veterinario);
+
+    /**
+     * Ejecuta una operación de actualización de un veterinario accediendo al JDBC.
+     * @param veterinario Veterinario a actualizar.
+     */
     void executeUpdateVeterinario(Veterinario veterinario);
+
+    /**
+     * Borra un veterinario de la base de datos.
+     * @param veterinario Veterinario a borrar.
+     */
     void deleteVeterinario(Veterinario veterinario); 
 }
